9fd184d2a2dc51ca7c755310689053258a37f627
Luk Gutierrez <lucianogutierrezagustin@gmail.com>
2023-02-08 16:24:52 -0300

    finishdesignbeta

--- lib/Home_Page.dart
+++ lib/Home_Page.dart
@@ -16,12 +16,16 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
+          Divider(
+            height: 6.0,
+            color: Colors.transparent,
+          ),
           Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Text("Español"),
               Divider(
-                height: 30,
+                height: 40,
                 color: Colors.transparent,
               ),
               SizedBox(
@@ -32,67 +36,84 @@ class _HomePageState extends State<HomePage> {
           ),
 
           ////////////////////PART N° 3///////////////////////
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextField(
-                    obscureText: true,
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                TextField(
+                    cursorColor: Colors.black,
                     decoration: InputDecoration(
                         hintText: "Celular o correo electrónico",
                         border: OutlineInputBorder())),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        hintText: "Contraseña", border: OutlineInputBorder())),
-              ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Color(0xFFF1877f2)),
-                  onPressed: () async {
-                    await addData(datacontroller.text);
-                    //Navigator.pop(context);
-                  },
-                  child: Container(
-                    height: 50,
-                    width: 350,
-                    child: Center(child: Text("Iniciar sesión")),
-                  ))
-            ],
+                Divider(
+                  height: 20.0,
+                  color: Colors.transparent,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Contraseña",
+                      border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.green, width: 3))),
+                ),
+                Divider(
+                  height: 20.0,
+                  color: Colors.transparent,
+                ),
+                ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(primary: Color(0xFFF1877f2)),
+                    onPressed: () async {
+                      await addData(datacontroller.text);
+                      //Navigator.pop(context);
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 370,
+                      child: Center(child: Text("Iniciar sesión")),
+                    ))
+              ],
+            ),
           ),
           //////////////////////////////fINISH PART 3////////////////
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text("¿Olvidaste tu contraseña?"),
-              ElevatedButton(
-                  child: SizedBox(
-                    width: 350,
-                    child: Center(
-                      child: Text(
-                        "Crear cuenta nueva",
-                        style: TextStyle(color: Colors.black),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text("¿Olvidaste tu contraseña?"),
+                Divider(
+                  height: 15.0,
+                  color: Colors.transparent,
+                ),
+                ElevatedButton(
+                    child: SizedBox(
+                      width: 350,
+                      child: Center(
+                        child: Text(
+                          "Crear cuenta nueva",
+                          style: TextStyle(color: Colors.black),
+                        ),
                       ),
                     ),
-                  ),
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    elevation: 0.0,
-                    primary: Colors.red.withOpacity(0),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(2),
-                        ),
-                        side: BorderSide(color: Colors.black26)),
-                  )),
-              Container(
-                width: 80,
-                height: 80,
-                child: Image(image: AssetImage("assets/meta.png")),
-              )
-            ],
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0.0,
+                      primary: Colors.red.withOpacity(0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(2),
+                          ),
+                          side: BorderSide(color: Colors.black26)),
+                    )),
+                Container(
+                  width: 80,
+                  height: 80,
+                  child: Image(image: AssetImage("assets/meta.png")),
+                )
+              ],
+            ),
           ),
         ],
       ),
